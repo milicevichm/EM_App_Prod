36cac59b522f8428ceb2ef7956821e859aeff5d2
Michael Milicevich <milicevichm@gmail.com>
2015-03-16 12:07:20 -0400

    Added test script

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,4 @@ class ApplicationController < ActionController::Base
     render text: "hello world!"
   end
 
-
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
+    exec('python interpreter.py')
   end
 
   def about
