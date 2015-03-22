930ba7bdfdccb84d6a582f90cfb78ba59895015d
Michael Milicevich <milicevichm@gmail.com>
2015-03-22 12:50:49 -0400

    Added downsampled REDD mains and disaggregated data for display plot

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
-    @mains_small = MAINS_CSV[3..10803]
-
+    #@mains_small = MAINS_CSV[3..-1 ]
+    #@disag_output = DISAG_CSV[3..-1]
   end
 
   def about
--- config/initializers/chartkick.rb
+++ config/initializers/chartkick.rb
@@ -1,3 +1,6 @@
 require 'csv'
-MAINS_CSV = CSV.read('/home/group20/data/meter1.csv')
+mains = CSV.read('/home/group20/data/redd_downs.csv')
+disag = CSV.read('/home/group20/data/fridge_output.csv')
 
+MAINS_CSV = mains[720..960]
+DISAG_CSV = disag[720..960]
