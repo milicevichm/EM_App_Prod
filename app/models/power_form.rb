705c7a451f509b52ea9bebe396a5ef37afe3ebaa
Michael Milicevich <milicevichm@gmail.com>
2015-04-04 15:07:56 -0400

    Added date validation for input form

--- app/models/power_form.rb
+++ app/models/power_form.rb
@@ -1,2 +1,21 @@
 class PowerForm < ActiveRecord::Base
+
+  validate :date_range_correct?
+
+  def date_range_correct?
+
+    if tstart < DateTime.strptime('2011-04-18T09:22:00', '%Y-%m-%dT%H:%M:%S') or tstart > DateTime.strptime('2011-05-24T15:57:00', '%Y-%m-%dT%H:%M:%S')
+      errors.add(:tstart, 'Please enter a start date within a valid range.')
+    end
+
+    if tend < DateTime.strptime('2011-04-18T09:22:00', '%Y-%m-%dT%H:%M:%S') or tend > DateTime.strptime('2011-05-24T15:57:00', '%Y-%m-%dT%H:%M:%S')
+      errors.add(:tend, 'Please enter a end date within a valid range.')
+    end
+
+    if tstart > tend
+      errors.add(:tstart, 'Please ensure that your start date comes before your end date.')
+    end
+
+  end
+
 end
